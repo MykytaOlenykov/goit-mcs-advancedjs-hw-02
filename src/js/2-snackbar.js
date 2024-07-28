4a33391aabe2f30ff07c6971efdd7f4b7edd97cd
Mykyta <nikita2001395@gmail.com>
2024-07-28 12:14:05 +0300

    fix: task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,65 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import '../css/common.css';
+
+const formRef = document.querySelector('.form');
+
+const notify = {
+  success(message) {
+    iziToast.success({
+      position: 'topRight',
+      message,
+    });
+  },
+  error(message) {
+    iziToast.error({
+      position: 'topRight',
+      message,
+    });
+  },
+};
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const { delay, state } = e.target.elements;
+  const delayValue = delay.value.trim();
+  const stateValue = state.value;
+
+  if (!delayValue.trim() || !stateValue) {
+    notify.error('Fill please all fields');
+    return;
+  }
+
+  const normalizedDelayValue = Number(delayValue);
+  if (Number.isNaN(normalizedDelayValue)) {
+    notify.error(`Invalid delay: ${delayValue}`);
+    return;
+  }
+
+  if (!Number.isInteger(normalizedDelayValue)) {
+    notify.error('Delay must be integer');
+    return;
+  }
+
+  if (stateValue !== 'fulfilled' && stateValue !== 'rejected') {
+    notify.error(`Invalid state: ${stateValue}`);
+    return;
+  }
+
+  createPromise(stateValue, normalizedDelayValue)
+    .then(notify.success)
+    .catch(notify.error);
+});
+
+function createPromise(state, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
